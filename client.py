063d18e96e1ebb6b685115ec5fcec2152180a4a5
Larissa Rodrigues de Almeida <49217170+RodriguesLarissa@users.noreply.github.com>
2023-06-25 20:43:51 +0000

    Funcionality of download and update

--- client.py
+++ client.py
@@ -1,13 +1,17 @@
 import socket
 import json
 import os
-
-# Creation of a socket object
-socket_server = socket.socket()
-socket_peer = socket.socket()
+from threading import Thread
 
 # Define port to connect with server
 PORT = 1099
+
+socket_server = socket.socket()
+socket_peer = socket.socket()
+socket_download = socket.socket()
+folder_path = ""
+peer_ip = ""
+peer_port = 0
 
 def join():
     """ Join """
@@ -15,39 +19,68 @@ def join():
     peer_port = int(input("Digite a porta: "))
     peer_address = f"{peer_ip}:{peer_port}"
     folder_path = str(input(("Digite o nome da pasta com os arquivos: ")))
-    filenames = get_filenames(folder_path)
+    filenames = get_filenames()
+
+    socket_server.connect(('', PORT))
     message_to_server = {"type": "JOIN", "address": peer_address, "filenames": filenames}
-    server_message = connect_to_server(message_to_server)
+    server_message = send_message_to_server(socket_server, message_to_server)
     if server_message == "JOIN_OK":
         print(f'Sou peer {peer_address} com arquivos {filenames}')
-    socket_server.close()
 
 def search():
     """ Search """
     filename = str(input(("Digite o nome do arquivo que está procurando: ")))
-    peer_addresses = connect_to_server({"type": "SEARCH", "filename": filename})
+    message_to_server = {"type": "SEARCH", "filename": filename}
+    peer_addresses = send_message_to_server(socket_server, message_to_server)
     print(f'peers com arquivo solicitado: {json.loads(peer_addresses)}')
-    socket_server.close()
 
 def download():
     """ Download """
-    peer_address = (input("Digite o ip: "), int(input("Digite a porta: ")))
+    peer_ip = input("Digite o ip: ")
+    peer_port = int(input("Digite a porta: "))
+    peer_address = f"{peer_ip}:{peer_port}"
     filename = str(input(("Digite o nome do arquivo para download: ")))
-    connect_to_server({"type": "DOWNLOAD", "address": peer_address, "filename": filename})
-    socket_server.close()
 
-def connect_to_server(message_to_server: dict):
-    """ Connect """
-    socket_server.connect(('', PORT))
-    socket_server.sendall(json.dumps(message_to_server).encode())
-    return socket_server.recv(1024).decode()
+    socket_peer.connect(('', peer_port))
+    message_to_server = {"type": "DOWNLOAD", "address": peer_address, "filename": filename}
+    send_message_to_server(socket_peer, message_to_server)
+    print(f'Arquivo {filename} baixado com sucesso na pasta {folder_path}')
+    update(filename)
 
-def get_filenames(folder_path: str):
+def update(filename: str):
+    """ Update """
+    message_to_server = {"type": "UPDATE", "filename": filename}
+    send_message_to_server(socket_server, message_to_server)
+
+def send_message_to_server(socket: socket, message_to_server: dict):
+    """ Connect """    
+    socket.sendall(json.dumps(message_to_server).encode())
+    return socket.recv(1024).decode()
+
+def get_filenames():
     """ Get filenames """
     filenames = []
     for filename in os.listdir(folder_path):
         filenames.append(filename)
     return filenames
+
+def listen_connections():
+    """ Listen """
+    while True:
+        socket_download.accept()
+        thread = Thread(target=process_message)
+        thread.start()
+
+def process_message():
+    """ Listen """
+    while True:
+        socket_download.bind(('', peer_port))
+        socket_download.listen(5)
+        filename = json.loads(socket_download.recv(1024).decode())
+
+        with open(folder_path + filename, "rb") as f:
+            bytes_read = f.read()
+            socket_download.sendall(bytes_read)
 
 while True:
     action = int(input('''Escolha uma ação:  
--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 import socket
 import json
+from threading import Thread
 
 # Reserve a port
 PORT = 1099
 
 #Relation of file names with the peer address
 relation_file_names_peers = {}
+relation_peers_address = {}
 
 # Creation of a socket object
 s = socket.socket()
@@ -19,29 +21,32 @@ print(f"Socket binded to {(PORT)}")
 s.listen(5)
 print("Socket is listening")
 
+
+def process_message(socket: socket, address: tuple):
+    """ Process Message """
+    while True:
+        # Receive request from the client
+        request = json.loads(c.recv(1024).decode())
+
+        # Check type of request
+        match request["type"]:
+            case "JOIN":
+                relation_file_names_peers.update({request["address"]: request["filenames"]})
+                relation_peers_address.update({address: request["address"]})
+                c.send("JOIN_OK".encode())
+                print(f'Peer {request["address"]} adicionado com arquivos {request["filenames"]}')
+
+            case "SEARCH":
+                peers_with_file = [peer for peer in relation_file_names_peers if request["filename"] in relation_file_names_peers[peer]]
+                c.sendall(json.dumps(peers_with_file).encode())
+                print(f'Peer {relation_peers_address[address]} solicitou arquivo {request["filename"]}')
+                
+            case "UPDATE":
+                relation_file_names_peers[addr].extend(request.file_name)
+                c.send("UPDATE_OK".encode())
+
 while True:
     # Establish connection with client
     c, addr = s.accept()
-
-    # Receive request from the client
-    request = json.loads(c.recv(1024).decode())
-    print(request)
-
-    # Check type of request
-    match request["type"]:
-        case "JOIN":
-            relation_file_names_peers.update({request["address"]: request["filenames"]})
-            c.send("JOIN_OK".encode())
-            print(f'Peer {request["address"]} adicionado com arquivos {request["filenames"]}')
-
-        case "SEARCH":
-            peers_with_file = [peer for peer in relation_file_names_peers if request["filename"] in relation_file_names_peers[peer]]
-            c.sendall(json.dumps(peers_with_file).encode())
-            print(f'Peer {request["address"]} solicitou arquivo {request["filename"]}')
-            
-        case "UPDATE":
-            relation_file_names_peers[addr].extend(request.file_name)
-            c.send("UPDATE_OK".encode())
-    
-    # Close the connection with the client
-    c.close()
+    thread = Thread(target=process_message, args=(c, addr))
+    thread.start()
